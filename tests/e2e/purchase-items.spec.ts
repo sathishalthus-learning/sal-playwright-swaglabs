fbf4dfb91bb7254e2b05540b95be17a70f74572e
sathishalthus-learning <sathishalthus.learning@gmail.com>
2025-03-18 16:42:53 +0530

    e2e

--- tests/e2e/purchase-items.spec.ts
+++ tests/e2e/purchase-items.spec.ts
@@ -1,45 +1,13 @@
-import test, { expect } from "@playwright/test";
-import { LoginPage } from "../pages/1-login-page";
-import { CatalogPage } from "../pages/2-catalog-page";
-import { CartPage } from "../pages/3-order-items-page";
-import { ContactPage } from "../pages/4-delivery-info-page";
-import { OrderPage } from "../pages/5-place-order-page";
-import { SuccessPage } from "../pages/6-order-success-page";
-import { describe } from "node:test";
+import { test, expect } from "../fixtures/fixtures.ts";
 
-describe('order-items', async () => {
+test.describe("order-items", async () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/v1/");
+  });
 
-    test.beforeEach(async ({ page }) => {
-        await page.goto('https://www.saucedemo.com/v1/');
-
-
-    });
-
-    test('buy a product', async ({ page }) => {
-        //
-        const loginPage = new LoginPage(page);
-        const username: string = 'standard_user';
-        const password: string = 'secret_sauce'
-        await loginPage.loginToSauceDemo('standard_user', 'secret_sauce');
-        //
-        const catalogPage = new CatalogPage(page);
-        const items: string[] = ['Sauce Labs Backpack'];
-        await catalogPage.addItemsToCart(items);
-        await catalogPage.verifyCartCount('1');
-        await catalogPage.goToCart();
-        //
-        const cartPage = new CartPage(page);
-        await cartPage.checkoutCart();
-        //
-        const contactInfo=['Rosee','Selva','60000'];
-        const contactPage = new ContactPage(page);
-        await contactPage.fillContactInfo(contactInfo);
-        //
-        const orderPage = new OrderPage(page);
-        await orderPage.completeOrder();
-        //
-        const successPage = new SuccessPage(page);
-        await successPage.verifySuccessMessage();
-
-    });
+  test("buy a product", async ({ page, loginPage, catalogPage }) => {
+    await loginPage.loginToSauceDemo("standard_user", "secret_sauce");
+    await page.waitForTimeout(5000);
+    expect(await catalogPage.getHeading()).toBe("Products");
+  });
 });
---
+++ tests/fixtures/fixtures.ts
@@ -0,0 +1,39 @@
+import { LoginPage } from "../pages/1-login-page";
+import { CatalogPage } from "../pages/2-catalog-page";
+import { CartPage } from "../pages/3-order-items-page";
+import { ContactPage } from "../pages/4-delivery-info-page";
+import { OrderPage } from "../pages/5-place-order-page";
+import { SuccessPage } from "../pages/6-order-success-page";
+import { test as baseTest } from "@playwright/test";
+
+type fixtures = {
+  loginPage: LoginPage;
+  catalogPage: CatalogPage;
+  cartPage: CartPage;
+  contactPage: ContactPage;
+  orderPage: OrderPage;
+  successPage: SuccessPage;
+};
+
+export const test = baseTest.extend<fixtures>({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+  catalogPage: async ({ page }, use) => {
+    await use(new CatalogPage(page));
+  },
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page));
+  },
+  contactPage: async ({ page }, use) => {
+    await use(new ContactPage(page));
+  },
+  orderPage: async ({ page }, use) => {
+    await use(new OrderPage(page));
+  },
+  successPage: async ({ page }, use) => {
+    await use(new SuccessPage(page));
+  },
+});
+
+export { expect } from "@playwright/test";
